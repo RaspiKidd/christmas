efb9568aee94f4a141b90e56156d33507091d882
Kerry Kidd <kerry@raspikidd.com>
2021-12-23 12:39:28 +0000

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,40 @@
+function JingleBells () {
+    for (let index = 0; index < 2; index++) {
+        music.playTone(330, music.beat(BeatFraction.Whole))
+        music.playTone(330, music.beat(BeatFraction.Whole))
+        music.playTone(330, music.beat(BeatFraction.Whole))
+        basic.pause(100)
+    }
+    music.playTone(330, music.beat(BeatFraction.Whole))
+    music.playTone(392, music.beat(BeatFraction.Whole))
+    music.playTone(262, music.beat(BeatFraction.Whole))
+    music.playTone(294, music.beat(BeatFraction.Whole))
+    music.playTone(330, music.beat(BeatFraction.Whole))
+    basic.pause(200)
+    for (let index = 0; index < 5; index++) {
+        music.playTone(349, music.beat(BeatFraction.Whole))
+    }
+    music.playTone(330, music.beat(BeatFraction.Whole))
+    music.playTone(330, music.beat(BeatFraction.Whole))
+    music.playTone(330, music.beat(BeatFraction.Half))
+    music.playTone(330, music.beat(BeatFraction.Half))
+}
+basic.forever(function () {
+    basic.showString("MERRY CHRISTMAS!")
+    basic.pause(500)
+    JingleBells()
+    music.playTone(330, music.beat(BeatFraction.Whole))
+    music.playTone(294, music.beat(BeatFraction.Whole))
+    music.playTone(294, music.beat(BeatFraction.Whole))
+    music.playTone(330, music.beat(BeatFraction.Whole))
+    music.playTone(294, music.beat(BeatFraction.Whole))
+    basic.pause(100)
+    music.playTone(392, music.beat(BeatFraction.Whole))
+    basic.pause(100)
+    JingleBells()
+    music.playTone(392, music.beat(BeatFraction.Whole))
+    music.playTone(392, music.beat(BeatFraction.Whole))
+    music.playTone(349, music.beat(BeatFraction.Whole))
+    music.playTone(294, music.beat(BeatFraction.Whole))
+    music.playTone(262, music.beat(BeatFraction.Whole))
+})
